7d0bd23af8981457967bbdd7c53a9b3b817df653
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-04-13 15:39:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,21 +1,21 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginningOfArray(array, x) {
+function addElementToBeginningOfArray(array, element) {
   var myArray = array;
-  return [x, ...myArray]
+  return [element, ...myArray]
 }
 
-function destructivelyAddElementToBeginningOfArray(array, x) {
-  return array.unshift(x);
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element);
 }
 
-function addElementToEndOfArray(array,x) {
+function addElementToEndOfArray(array,element) {
   var myArray = array;
-  return [...myArray, x]
+  return [...myArray, element]
 }
 
-function destuctivelyAddElementToEndOfArray(array, x) {
-  return array.push(x);
+function destuctivelyAddElementToEndOfArray(array, element) {
+  return array.push(element);
 }
 
 function accessElementInArray(array, indexNum) {
